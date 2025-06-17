80c46ea17078383573daf0df261af18b0ed6690d
lbg75 <jihunhyeon376@gmail.com>
2025-06-17 20:48:58 +0900

    오늘 원범초이
    ㅃㅂㅇㅇㅃㅃㅂㅉㅉㅈㄸㄸㄲㄷㄲㄲㅁㅁㅁㅁㅃㅂㅈㄷㄱㅂㅅㅈㄷㅂㅅㅈㄷㅂㅈㅆㅆㅂㅅㅂㅆ쇼ㅛㅛㅛㅛㅛㅅㄷㅆㄷㅆㅆㅆㅆㄲㄲㄲㄲㄲㄲㄲㄱㄸㄷㄸㅉㅃㄸㅉㅃㄸㅉㅃㅉㄸㅃㄸㅃㄲㄸㅉ

--- ohyun-ict-2025-main/api/app.js
+++ ohyun-ict-2025-main/api/app.js
@@ -1,36 +1,39 @@
-//https://crudcrud.com/api/1eb25b6027634314ab09e1168c6d903f
-
-// fetch("https://crudcrud.com/api/1eb25b6027634314ab09e1168c6d903f/post").then((res)=>{
-// return res.json()
-// }).then((json)=>{
-//     console.log(json)
-// })
-
-// async function init(){}
-const post_url = "https://crudcrud.com/api/1eb25b6027634314ab09e1168c6d903f/post"
+const post_url = "http://crud.tlol.me/wonbeom/post";
+let pagePointer = 0
 const init = async ()=>{
     const res = await fetch(post_url);
     const json = await res.json();
-    console.log(json);
-    json.forEach(post => {
-        const postli = document.createElement("li");
-        postli.innerText = post.title;
-        document.querySelector("#board").appendChild(postli);
+    const createPost = (post) => {
+        const postLi = document.createElement("li");
+        postLi.classList.add("post-container");
+        document.querySelector("#board").appendChild(postLi);
+
+        const postTitle = document.createElement("span");
+        postTitle.innerText = post.title;
+        postTitle.classList.add("bold-text");
+        postLi.appendChild(postTitle);
+
         const contentDiv = document.createElement("div");
         contentDiv.innerText = post.content;
-        postli.appendChild(contentDiv);
-        contentDiv.style.display = "none";
-        
-        postli.addEventListener("click",()=>{
-            if (contentDiv.style.display == "block") {
-                contentDiv.style.display = "none";
-            } else {
-                contentDiv.style.display = "block";
+        postLi.appendChild(contentDiv);
+        contentDiv.style.display = 'none';
+        postLi.addEventListener('click',()=>{
+            if (contentDiv.style.display == 'block') {
+                contentDiv.style.display = 'none';
+            }else{
+                contentDiv.style.display = 'block';
             }
-        });
-
-    });
+        }); 
+    }
+    json.slice(0,5).forEach(createPost);
+    pagePointer+=5
+    document.querySelector("#post-more").addEventListener("click",()=>{
+        const prevPointer = pagePointer;
+        pagePointer = pagePointer+5
+        json.slice(prevPointer,pagePointer).forEach(createPost)
+    })
 }
+
 const write = async (data)=>{
     const res = await fetch(post_url,{
         method:"POST",
@@ -56,20 +59,13 @@ document.querySelector("#post-form").addEventListener("submit",(e)=>{
     e.preventDefault()
     submit();
 })
-document.querySelector("#writing-start").addEventListener("click",()=>{
-    if (document.querySelector("#post-form").style.display == "block") {
-        document.querySelector("#post-form").style.display = "none"
-    } else {
-        document.querySelector("#post-form").style.display = "block"
-    }
-})
-document.querySelector("#board-start").addEventListener("click",()=>{
-    if (document.querySelector("#board").style.display == "block") {
-        document.querySelector("#board").style.display = "none"
-    } else {
-        document.querySelector("#board").style.display = "block"
-    }
-})
 
+document.querySelector("#toggle-write").addEventListener("click",()=>{
+    if(document.querySelector("#post-form").style.display == 'block'){
+        document.querySelector("#post-form").style.display = 'none'
+    }else{
+        document.querySelector("#post-form").style.display = 'block'
+    }
+});
 
 init()
